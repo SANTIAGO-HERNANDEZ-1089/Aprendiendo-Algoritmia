3a7312c5b3e42e38066ed24c29aa76e3178b4515
Santiago Hernandez <hernnadezsantiago.00@gmail.com>
2026-06-08 14:22:32 -0500

    Subo el primer ejercicio de phyton

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+numeroy = input("ingrese el primer numero")
+numerox = input("ingrese el segundo numero")
+
+numero1 = int (numeroy)
+numero2 = int (numerox)
+
+suma = int(numero1 + numero2)
+resta = int(numero1 - numero2)
+mult = int(numero1 * numero2)
+div = int(numero1 / numero2)
+pot = int(numero1 ** numero2)
+
+print(suma)
+print(resta)
+print(mult)
+print(div)
+print(pot)
