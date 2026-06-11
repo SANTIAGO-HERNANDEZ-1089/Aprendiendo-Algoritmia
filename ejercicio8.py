0d98db741f340b461345f8b2a88391751e09e66a
Santiago Hernandez <hernnadezsantiago.00@gmail.com>
2026-06-11 08:26:29 -0500

    agrego ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
+
+suma = num1 + num2 
+if suma % 2 == 0:
+    print("La suma es de resultado PAR: ", suma)
+else :
+    print("La suma es de resultado IMPAR: ", suma)
