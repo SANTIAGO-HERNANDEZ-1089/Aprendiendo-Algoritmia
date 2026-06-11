7eac1210bfb73d0fb2c6733be9d254fc5d8ae6f2
Cristians Marmolejo <CFMR25886781@soy.sena.edu.co>
2026-06-11 07:36:56 -0500

    Agrego ejercicio 9 de pytohn

---
+++ ejercicio9.py
@@ -0,0 +1,9 @@
+pesos = int(input("ingresas los pesos a convertir"))
+
+dolares = int (pesos * 1 / 3777)
+euros = int (pesos * 1 / 4364)
+
+print("pesos convertidos a dolares:", dolares)
+print("pesos convertidos a euros:", euros)
+
+
