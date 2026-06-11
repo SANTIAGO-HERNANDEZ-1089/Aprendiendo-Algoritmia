3f3a2cf20fbd87db3e83baf9423c8eb996e59284
Santiago Hernandez <hernnadezsantiago.00@gmail.com>
2026-06-10 21:13:57 -0500

    modifico ele ejercicio 3 python

---
+++ ejercicio3.py
@@ -0,0 +1,10 @@
+temperatura = float(input("ingrese una temperatura:"))
+escala = input("es Fahrenheit (f) o es centígrados (c): ").lower()
+if escala == "F" :
+    centígrados = temperatura - 32 * 5 / 9
+    print(centígrados)
+elif escala == "C":
+    Fahrenheit = temperatura * 1.8 + 32
+    print(Fahrenheit)
+else :
+    print("escala incorrecta")
